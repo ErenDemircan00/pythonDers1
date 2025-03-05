5af42c1995652efb2400293a5f8b23b7915058a0
Eren Demircan <154807775+ErenDemircan00@users.noreply.github.com>
2025-03-05 15:51:28 +0300

    değişkenler

---
+++ app.py
@@ -0,0 +1,15 @@
+# print('merhaba benim adım {}'.format("eren"))
+# print('merhaba benim adım {0},yasim {1}'.format("eren",21))
+# print('merhaba benim adım {1},yasim {0}'.format("eren",21))
+# print('merhaba benim adım {ad},yasim {yas}'.format(ad="eren",yas=21))
+
+# ders 2 ---------------------------------------------------------------
+
+# sayi =11
+# print(sayi)
+# sayi=sayi+4
+# print(sayi)
+
+# ders 3 ---------------------------------------------------------------
+
+print(type(2))
